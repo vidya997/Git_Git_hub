27a1e0464d1ed6268c7e0300911c79d75512c963
vidya997 <vidyashri13044@gmail.com>
2023-02-17 19:31:09 +0530

    this is second commit

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST']) # To render Homepage
+@app.route('/', methods=['GET', 'POST']) # To render Homepage  # multiplr code
 def home_page():
     return render_template('index.html')
 
-@app.route('/math', methods=['POST'])  # This will be called from UI
+@app.route('/math', methods=['POST'])  # This will be called from UI 
 def math_operation():
     if (request.method=='POST'):
         operation=request.form['operation']
